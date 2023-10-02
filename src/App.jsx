85349207b3d2679ac08c73e008f3aa48b8427b5e
Nathan Turkson <nathanturkson@gmail.com>
2023-10-02 10:32:35 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import QRcodecomponent from "./components/QRcodecomponent";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      
+
+    <QRcodecomponent  />
+
+
+
+      <div className="attribution">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://nathanturksonsite.netlify.app">Nathan Turkson</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QRcodecomponent.jsx
@@ -0,0 +1,12 @@
+import "./QRcodecomponent.css"
+
+function QRcodecomponent() {
+  return (
+    <article className="QRCODE">
+      Improve your front-end skills by building projects Scan the QR code to
+      visit Frontend Mentor and take your coding skills to the next level
+    </article>
+  );
+}
+
+export default QRcodecomponent
