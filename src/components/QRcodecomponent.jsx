b0f4f5d8ca70e42ea9a0710d87976dcdc9345d23
Nathan Turkson <nathanturkson@gmail.com>
2023-10-03 09:37:51 +0000

    Final changes

--- src/components/QRcodecomponent.jsx
+++ src/components/QRcodecomponent.jsx
@@ -1,12 +1,19 @@
-import "./QRcodecomponent.css"
+import "./QRcodecomponent.css";
+import QRImage from "../assets/images/image-qr-code.png";
 
 function QRcodecomponent() {
   return (
     <article className="QRCODE">
-      Improve your front-end skills by building projects Scan the QR code to
-      visit Frontend Mentor and take your coding skills to the next level
+      <img src={QRImage} alt="" />
+      <div className="desciption">
+        <h1> Improve your front-end skills by building projects</h1>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
     </article>
   );
 }
 
-export default QRcodecomponent
+export default QRcodecomponent;
